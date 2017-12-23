2ddc949893c660842b1bfa9c86f5896e504faef2
AlexanderTarasov11-708 <aleksandrtarasov53@gmail.com>
2017-12-23 17:13:35 +0300

    Update Alien.cs

--- SpaceInvaders/Alien.cs
+++ SpaceInvaders/Alien.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,31 +10,29 @@ namespace SpaceInvaders
     {
         public int X;
         public int Y;
-        //public int hp = 1;
+        public int move;
 
         public Alien(int y, int x) { X = x; Y = y; }
 
         public void Act()
         {
             if (Y == 18) Game.EndGame = true;
-            int move = Game.Rand.Next(1, 33);
-            switch (move)
+            int variant = Game.Rand.Next(1, 3);
+            if (move != 0 && move % 50 == 0)
             {
-                case 1:
+                if (variant == 1)
                     GoTo(0, 1);
-                    break;
-                case 2:
-                    GoTo(-1, 0);
-                    break;
-                case 3:
-                    GoTo(1, 0);
-                    break;
-                case 4:
-                    if (Y+1 < Game.Map.GetLength(0))
-                    Game.Map[Y + 1, X] = new AlienBullet(Y + 1, X);
-                    break;
-
+                else if (variant == 2)
+                    GoTo(-1, 1);
+                else if (variant == 3)
+                    GoTo(1, 1);
             }
+            if (move % 15 == 0)
+            {
+                if (Y + 1 < Game.Map.GetLength(0))
+                    Game.Map[Y + 1, X] = new AlienBullet(Y + 1, X);
+            }
+            move++;
         }
 
         public void GoTo(int deltaX, int deltaY)
